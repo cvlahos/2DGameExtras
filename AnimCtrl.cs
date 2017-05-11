935a567d3bcb4cbf81c2dabcf07d9148e04734ed
cvlahos <cvlahos@users.noreply.github.com>
2017-05-10 20:29:51 -0400

    Add files via upload

---
+++ AnimCtrl.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimCtrl : MonoBehaviour 
+{
+	public Animator animatorForPaddle;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+		if (Input.GetKeyDown(KeyCode.LeftArrow)) 
+		{
+
+			animatorForPaddle.Play ("fancyLeft");
+
+		}
+	
+	
+		if (Input.GetKeyDown(KeyCode.RightArrow)) 
+		{
+			animatorForPaddle.Play ("fancyRight");
+		}
+	
+	}
+}
