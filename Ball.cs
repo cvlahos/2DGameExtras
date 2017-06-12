d929e1bb2174ee213f685f54acb764a3b5c7b2d0
cvlahos <cvlahos@users.noreply.github.com>
2017-06-12 11:43:20 -0400

    Add files via upload

---
+++ Ball.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour 
+{
+	
+	Rigidbody2D rb;
+	public float ballSpeed;
+	public float boosterSpeed;
+	
+	public GameManager gameManager;
+	
+	public int boostCount;
+
+	public CircleCollider2D ballCircCollider;
+
+	Vector2 constantBallSpeed;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		rb = GetComponent<Rigidbody2D> ();
+		ballCircCollider.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+
+		ballSpeedManager ();
+
+	}
+
+	void ballSpeedManager()
+	{
+		Vector2 constantBallSpeed = rb.velocity.normalized * ballSpeed;
+		
+		rb.velocity = constantBallSpeed;
+
+	}
+	
+
+	public void LauchBall()
+	{
+		ballCircCollider.enabled = true;
+		transform.parent = null;
+		rb.velocity = new Vector2 (0,ballSpeed * Time.deltaTime);
+		
+	}
+	
+	
+	void OnCollisionEnter2D()
+	{
+		if (boostCount < gameManager.ballBoostLimit) 
+		{
+			BoostSpeed ();
+		}
+		
+	}
+	
+	void BoostSpeed()
+	{
+		boostCount++;
+		ballSpeed = ballSpeed + boosterSpeed;
+	}
+	
+}
---
+++ BallControl.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallControl : MonoBehaviour 
+{
+	Rigidbody ballRB;
+	TrailRenderer ballTrail;
+	Transform paddleTR;
+
+	public float ballLaunchSpeed;
+
+	float ballMagnitude;
+
+	//MusicManager musicManager;
+
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		ballRB = GetComponent<Rigidbody> ();
+		ballTrail = GetComponent<TrailRenderer> ();
+		paddleTR = GameObject.Find ("Paddle").GetComponent<Transform> ();
+
+
+
+		//musicManager = FindObjectOfType<MusicManager> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		ForceDetector ();
+		//ballMagnitude = ballRB.velocity.magnitude;
+		//slowBallBooster ();
+		//Debug.Log (ballMagnitude);
+	}
+	public void LaunchBall()
+	{
+
+		transform.parent = null;
+		ballRB.AddForce (ballLaunchSpeed,0,0);
+		PaddleCTRL.isBallFired = true;
+		PaddleCTRL.isForceOn = false;
+
+	}
+
+	/*void OnTriggerEnter (Collider tri)
+	{
+		Debug.Log ("Trigger");
+	}
+	
+	void OnCollisionEnter (Collision col)
+	{
+		Debug.Log ("Collision");
+	}
+	*/
+
+	void OnCollisionEnter (Collision col)
+	{
+
+	}
+
+	public void ForceDetector()
+	{
+		if (PaddleCTRL.isForceOn == true) 
+		{
+			ballTrail.enabled = true;
+		}
+		else if (PaddleCTRL.isForceOn == false)
+		
+		{
+
+			ballTrail.enabled = false;
+		}
+	}
+
+
+
+	/*
+	 //will detect magnitude and make it faster heading towards the paddle if the ball falls under a certain number
+	void slowBallBooster()
+	{
+		if (ballMagnitude < 200f && PaddleCTRL.isBallFired == true) 
+		{
+
+			ballRB.velocity += new Vector3 (paddleTR.position.x, paddleTR.position.y, transform.position.z) * 5 * Time.deltaTime;
+		}
+
+	}
+	*/
+}
