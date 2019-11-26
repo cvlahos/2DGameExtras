ca51aea9938a2ba96c294da2f20bd2bb2c0a9963
cvlahos <cvlahos@users.noreply.github.com>
2019-11-26 13:39:56 -0500

    Add files via upload

---
+++ CoroutineEx.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoroutineEx : MonoBehaviour
+{
+    
+
+    void Start()
+    {
+        StartCoroutine(MyCoroutine());
+    }
+
+
+    IEnumerator MyCoroutine()
+    {     
+      
+        print("do something 1");
+        yield return new WaitForSeconds(5f);
+        print("do something 2");
+
+        yield return new WaitForSeconds(6f);
+
+        print("MyCoroutine is now finished.");
+
+        yield return null; // also use this in a loop to not lock up unity
+    }
+}
