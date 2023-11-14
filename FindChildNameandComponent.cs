c4d7f35c8d662378c8335873aa0aadd50bc7750b
cvlahos <cvlahos@users.noreply.github.com>
2023-11-14 09:55:07 -0500

    Add files via upload

---
+++ FindChildNameandComponent.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FindChildNameandComponent : MonoBehaviour
+{
+    Text myText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myText = GameObject.Find("Canvas").GetComponentInChildren<Transform>().Find("Text (10)").GetComponent<Text>();
+        myText.text = "Hey Hey";
+        // gameobject.GetComponentInChildren<Transform>().Find(Name).GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
